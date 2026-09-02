f5abdddd0c5c186c007b25f8e801e850385d3595
nikhil-aum <nikhilpatidar.aum@outlook.com>
2026-09-02 17:58:15 +0530

    add some logs in controller

--- src/main/java/com/nikhil/BankingApplication/controller/DepositController.java
+++ src/main/java/com/nikhil/BankingApplication/controller/DepositController.java
@@ -7,6 +7,8 @@ import com.nikhil.BankingApplication.service.DepositService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = " Deposit", description = "Deposit APIs")
 public class DepositController {
     private final DepositService depositService;
+    private static final Logger logger = LoggerFactory.getLogger(DepositController.class);
+
 
     public DepositController(DepositService depositService) {
         this.depositService = depositService;
@@ -30,6 +34,9 @@ public class DepositController {
     public ResponseEntity<TransactionResultDTO> deposit(@Valid @RequestBody TransactionRequestDTO request, Authentication authentication) {
 
         String email = authentication.getName();
+        logger.info("Deposit request received for account {} by user {} with amount {}",
+                request.getAccountNumber(), email, request.getAmount());
+
         TransactionResultDTO response = depositService.deposit(request,email);
         return ResponseEntity.ok(response);
     }
